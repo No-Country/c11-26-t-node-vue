5ed85fbf50ed70b43a02e6cf49fd894bf72a3de3
Guorvo <lmcuchero@gmail.com>
2023-05-23 15:47:55 -0300

    footer, comienzo del navbar, welcome

--- CLIENT/src/Components/Home/Home.jsx
+++ CLIENT/src/Components/Home/Home.jsx
@@ -1,39 +1,48 @@
 import Footer from "../Footer/Footer";
+import logo from "../../../assets/logo.svg"
 import "./Home.less";
 
 function Home() {
   return (
-    <> <div className="home">
-      <section className="navbar">
-        <div className="left-navbar">
-          <img src="" alt="" />
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div className="right-navbar">
-          <input type="text" />
-          <a href=""></a>
-          <a href=""></a>
-        </div>
-      </section>
-      <section className="iconic">
-        <img src="" alt="" />
-        <h2></h2>
-        <section className="iconic-albums">
-        {/* Aca van los álbumes iconicos! */}
+    <>
+      <div className="home">
+        <section className="navbar">
+          <div className="left-navbar">
+            <img src={logo} alt="" />
+            <ul>
+              <li>Inicio</li>
+              <li>Catalogo</li>
+              <li>Artistas</li>
+            </ul>
+          </div>
+          <div className="right-navbar">
+            <input type="text" />
+            <a href=""></a>
+            <a href=""></a>
+          </div>
         </section>
-      </section>
-      <section className="catalog">
-        {/* Nose si hacer un nuevo estilo para el catalogo en tema de css, despues veo, pero esto ya lo hizo diego! */}
-      </section>
-      <section className="artists">
-        {/* Esto va a ser un embole, el infierno de flexbox, es sufrir esto nomas en tema de diseño, ya vere como le hago! */}
-      </section>
-      <Footer/>
-    </div> </>
+        <section className="welcome">
+          <div className="welcome-text">
+            <h3>Te damos la bienvenida, Nombre.</h3>
+            <h1>¡vamos a disfrutar de la música a lo grande!</h1>
+          </div>
+        </section>
+        <section className="iconic">
+          <img src="" alt="" />
+          <h2></h2>
+          <section className="iconic-albums">
+            {/* Aca van los álbumes iconicos! */}
+          </section>
+        </section>
+        <section className="catalog">
+          {/* Nose si hacer un nuevo estilo para el catalogo en tema de css, despues veo, pero esto ya lo hizo diego! */}
+        </section>
+        <section className="artists">
+          {/* Esto va a ser un embole, el infierno de flexbox, es sufrir esto nomas en tema de diseño, ya vere como le hago! */}
+        </section>
+      </div>
+      <Footer />
+    </>
   )
 }
 
